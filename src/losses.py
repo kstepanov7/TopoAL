d7fed9c16d6bf74cb5fefe4cce04321f1f37e55b
kstepanov7 <62134316+kstepanov7@users.noreply.github.com>
2020-12-24 10:17:18 +0300

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -9,13 +9,14 @@ class Loss_D(nn.Module):
     def __init__(self, loss = nn.BCELoss()):
         super(Loss_D, self).__init__()
         self.Loss = loss
+        self.device = device
 
     def forward(self, pred_D, labels_D, pred_xy):
 
         loss = 0 
         for i in range(len(pred_D)):
 
-            loss += self.Loss(pred_D[i], labels_D[i].float()) + torch.mean(torch.sum(-torch.log(pred_xy[i]), (2,3)))
+            loss += self.Loss(pred_D[i], labels_D[i].to(device).float()) + torch.mean(torch.sum(-torch.log(pred_xy[i]), (2,3)))
             #loss += (self.BCELoss(pred_D[i].sigmoid(), labels_D[i]))
             
         return loss
