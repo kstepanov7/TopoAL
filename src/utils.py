0e109ea0ef26c59620e4f3b2a17bf45bcb253b97
kstepanov7 <62134316+kstepanov7@users.noreply.github.com>
2020-12-25 15:32:33 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -63,3 +63,17 @@ def add_mask(img, mask):
       mask = np.array(mask)
       img[:,:,0] = mask*255
       return img
+
+def calc_iou(prediction, ground_truth, tr = None):
+
+    n_images = len(prediction)
+    if tr != None:
+      for i in range(n_images):
+        prediction[i][prediction[i] <= tr] = 0
+        prediction[i][prediction[i] > tr] = 1
+
+    intersection, union = 0, 0
+    for i in range(n_images):
+        intersection += np.logical_and(prediction[i] > 0, ground_truth[i] > 0).astype(np.float32).sum() 
+        union += np.logical_or(prediction[i] > 0, ground_truth[i] > 0).astype(np.float32).sum()
+    return float(intersection) / union
